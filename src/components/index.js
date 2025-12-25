ff91e81809fb107f3dea7452d9a7c2aa4ab63bfd
Pandelis <pandelis777@gmail.com>
2025-12-25 11:23:30 +0200

    After Video 737 - Add Job Page - Second Part

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,8 @@
 import Logo from './Logo';
 import FormRow from './FormRow';
+import FormRowSelect from './FormRowSelect';
 import Navbar from './Navbar';
 import SmallSidebar from './SmallSidebar';
 import BigSidebar from './BigSidebar';
 
-export { Logo, FormRow, Navbar, BigSidebar, SmallSidebar };
+export { Logo, FormRow, FormRowSelect, Navbar, BigSidebar, SmallSidebar };
--- src/pages/dashboard/AddJob.js
+++ src/pages/dashboard/AddJob.js
@@ -1,6 +1,125 @@
+import { FormRow, FormRowSelect } from "../../components";
+import Wrapper from "../../assets/wrappers/DashboardFormPage";
+import { useSelector, useDispatch } from "react-redux";
+import { toast } from "react-toastify";
+import { handleChange, clearValues } from "../../features/job/jobSlice";
+
 const AddJob = () => {
+  const {
+    isLoading,
+    position,
+    company,
+    jobLocation,
+    jobType,
+    jobTypeOptions,
+    status,
+    statusOptions,
+    isEditing,
+    editJobId,
+  } = useSelector((store) => store.job);
+
+  const dispatch = useDispatch();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!position || !company || !jobLocation) {
+      toast.error("Please fill out all fields");
+      return;
+    }
+  };
+
+  const handleJobInput = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+    dispatch(handleChange({ name, value }));
+    // dispatch handleChange action
+  };
+
   return (
-	<h1>AddJob</h1>
-  )
-}
-export default AddJob
+    <Wrapper>
+      <form className="form">
+        <h3>{isEditing ? "edit job" : "add job"}</h3>
+        <div className="form-center">
+          {/* position */}
+          <FormRow
+            type="text"
+            name="position"
+            value={position}
+            handleChange={handleJobInput}
+          />
+          {/* company */}
+          <FormRow
+            type="text"
+            name="company"
+            value={company}
+            handleChange={handleJobInput}
+          />
+          {/* jobLocation */}
+          <FormRow
+            type="text"
+            name="jobLocation"
+            labelText="Job Location"
+            value={jobLocation}
+            handleChange={handleJobInput}
+          />
+          {/* job status */}
+          <div className="form-row">
+            <label htmlFor="status" className="form-label">
+              status
+            </label>
+            <select
+              name="status"
+              id="status"
+              value={status}
+              onChange={handleJobInput}
+              className="form-select"
+            >
+              {statusOptions.map((itemValue, index) => {
+                return (
+                  <option key={index} value={itemValue}>
+                    {itemValue}
+                  </option>
+                );
+              })}
+            </select>
+          </div>
+          {/* job status */}
+          <FormRowSelect
+            name="status"
+            value={status}
+            handleChange={handleJobInput}
+            list={statusOptions}
+          ></FormRowSelect>
+          {/* job type */}
+          <FormRowSelect
+            name="jobType"
+            labelText="job type"
+            value={jobType}
+            handleChange={handleJobInput}
+            list={jobTypeOptions}
+          ></FormRowSelect>
+
+          <div className="btn-container">
+            <button
+              type="button"
+              className="btn btn-block clear-btn"
+              onClick={() => dispatch(clearValues())}
+            >
+              Clear
+            </button>
+
+            <button
+              type="submit"
+              className="btn btn-block submit-btn"
+              onClick={handleSubmit}
+              disabled={isLoading}
+            >
+              Submit
+            </button>
+          </div>
+        </div>
+      </form>
+    </Wrapper>
+  );
+};
+export default AddJob;
